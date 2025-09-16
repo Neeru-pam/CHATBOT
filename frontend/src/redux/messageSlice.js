f4217252c3b2e85b11ee9ff7837d8b56a0e238a6
Neerupam <neerupam.26@gmail.com>
2025-09-16 13:08:22 +0530

    again

--- frontend/src/redux/messageSlice.js
+++ frontend/src/redux/messageSlice.js
@@ -1,25 +1,15 @@
-import {createSlice} from "@reduxxjs/toolkit";
+import {createSlice} from "@reduxjs/toolkit";
 
 const messageSlice = createSlice({
     name:"message",
     initialState:{
         messages:null,
-        unreadMessages: {},
     },
     reducers:{
         setMessages:(state,action)=>{
             state.messages = action.payload;
-        },
-        setUnreadMessages:(state, action) => {
-            state.unreadMessages = action.payload;
-        },
-        decrementUnreadMessages:(state, action) => {
-            const senderId = action.payload;
-            if (state.unreadMessages[senderId]) {
-                delete state.unreadMessages[senderId];
-            }
         }
     }
 });
-export const {setMessages, setUnreadMessages, decrementUnreadMessages} = messageSlice.actions;
+export const {setMessages} = messageSlice.actions;
 export default messageSlice.reducer;
